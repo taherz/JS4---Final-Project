8f2645f23815bb207e1b66e1f13ab76e67452a2e
Taher Alzahrani <taher.alzahrani@hotmail.com>
2019-12-11 18:52:43 +0300

    - add keys :)

---
+++ js/keys.js
@@ -0,0 +1,15 @@
+var config = {
+    firebaseConfig: function () {
+        return {
+            apiKey: "AIzaSyCRos37YTbfzLP2eOO7wOEF9vHoUp_7rp4",
+            authDomain: "js4-finalproject.firebaseapp.com",
+            databaseURL: "https://js4-finalproject.firebaseio.com",
+            projectId: "js4-finalproject",
+            storageBucket: "js4-finalproject.appspot.com",
+            messagingSenderId: "981478231483",
+            appId: "1:981478231483:web:87daa570e95f62aae7889e"
+        };
+    },
+    newsApiKey: "15dc724323194d68b22af0dd71c3e5ed",
+    exportSubscribtionListPass:"JavaScript"
+};
